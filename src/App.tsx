2c2390f93d9d9ff04e163a98608abd58ab130d16
liuyang <liuyang@hdfax.com>
2018-03-01 10:40:55 +0800

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import './App.css';
+import Hello from './components/Hello';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <Hello name="TypeScript" />
+    </div>
+  );
 }
 
 export default App;
